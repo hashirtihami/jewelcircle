3a5a6cde78167cea0a3052a8b2231051737fa15c
elishaJ <elisha.jamil@gmail.com>
2018-07-21 18:27:39 +0500

    more jquery

--- viewForm.js
+++ viewForm.js
@@ -20,43 +20,49 @@ function displayNewType() {
 	$("#newType input").prop('required',true);
 }
 function displayPriceInputGold() {
-	var tar = document.getElementById("priceInputGold"); 
-	tar.classList.toggle("hideShow");
+	// var tar = document.getElementById("priceInputGold"); 
+	// tar.classList.toggle("hideShow");
+	$("#priceInputGold").toggle("fast");
 	if(!($("#priceInputGold").hasClass("hideShow"))){
 		$("#Gold").prop('required',true);
 	}
 }
 function displayPriceInputSilver() {
-	var tar = document.getElementById("priceInputSilver"); 
-	tar.classList.toggle("hideShow");
+	// var tar = document.getElementById("priceInputSilver"); 
+	// tar.classList.toggle("hideShow");
+	$("#priceInputSilver").toggle("fast");
 	if(!($("#priceInputSilver").hasClass("hideShow"))){
 		$("#Silver").prop('required',true);
 	}
 }
 function displayPriceInputEnglish() {
-	var tar = document.getElementById("priceInputEnglish"); 
-	tar.classList.toggle("hideShow");
+	// var tar = document.getElementById("priceInputEnglish"); 
+	// tar.classList.toggle("hideShow");
+	$("#priceInputEnglish").toggle("fast");
 	if(!($("#priceInputEnglish").hasClass("hideShow"))){
 		$("#English").prop('required',true);
 	}
 }
 function displayPriceInputUrdu() {
-	var tar = document.getElementById("priceInputUrdu"); 
-	tar.classList.toggle("hideShow");
+	// var tar = document.getElementById("priceInputUrdu"); 
+	// tar.classList.toggle("hideShow");
+	$("#priceInputUrdu").toggle("fast");
 	if(!($("#priceInputUrdu").hasClass("hideShow"))){
 		$("#Urdu").prop('required',true);
 	}
 }
 function displayPriceInputArabic() {
-	var tar = document.getElementById("priceInputArabic"); 
-	tar.classList.toggle("hideShow");
+	// var tar = document.getElementById("priceInputArabic"); 
+	// tar.classList.toggle("hideShow");
+	$("#priceInputArabic").toggle("fast");
 	if(!($("#priceInputArabic").hasClass("hideShow"))){
 		$("#Arabic").prop('required',true);
 	}
 }
 function displayPriceInputDouble() {
-	var tar = document.getElementById("priceInputDouble"); 
-	tar.classList.toggle("hideShow");
+	// var tar = document.getElementById("priceInputDouble"); 
+	// tar.classList.toggle("hideShow");
+	$("#priceInputDouble").toggle("fast");
 	if(!($("#priceInputDouble").hasClass("hideShow"))){
 		$("#Double").prop('required',true);
 	}
@@ -82,7 +88,6 @@ function checkInput() {
 	chainInput.style.display = "none";
 	var inputCheck = val.options[val.selectedIndex].value;
 	if((inputCheck === "bracelet") || (inputCheck === "locket")) {
-		chainInput
 		chainInput.style.display = "block";
 	}
 }
