64b06fa1f7f8d15989df242a352f2ade4606ff71
elishaJ <elisha.jamil@gmail.com>
2018-07-27 00:38:06 +0500

    allproducts.php

---
+++ adminPHP/allProducts.php
@@ -0,0 +1,10 @@
+<?php
+  require 'templates/top.inc.php';
+  require 'allProductsContent.php';
+  require 'popup.php';
+  require 'templates/bottom.inc.php';
+  require 'connect.inc.php';
+?>
+
+<link rel="stylesheet" type="text/css" href="utils.css">
+<script src="js/giftcard.js"></script>
