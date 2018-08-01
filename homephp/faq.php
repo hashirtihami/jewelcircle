f762f3e49e0c6d6cc97942a2eb3557e1c826e1d7
tihaami <hashirtihamipk@gmail.com>
2018-08-01 20:17:22 +0500

    trms

---
+++ homephp/faq.php
@@ -0,0 +1,146 @@
+ <?php
+	require 'templates/top.inc.php';
+?>
+
+<!-- -------------------- bs head hata dy sb set ho jayega --------------------------------------------- 
+<head>
+ 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+-->
+
+<link href="themes/css/footerlinks.css" rel="stylesheet">
+
+<div class="container">
+  <h2>FAQ</h2>
+  <div class="panel-group" id="accordion">
+    
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">How do I order?</a>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse  ">
+        <div class="panel-body">Simply click the "Add to Cart" button, and then go to your cart:
+				After that, please click "check out", and fill in your details or use your log in info.
+		</div>
+      </div>
+    </div>
+    
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Do you ship international? </a>
+        </h4>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body">Yes we ship World-Wide</div>
+      </div>
+    </div>
+    
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Do you ship items to Pakistan? </a>
+        </h4>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+        <div class="panel-body">Yes. We ship to over 55 cities in Pakistan.</div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">How long does shipping usually take?  </a>
+        </h4>
+      </div>
+      <div id="collapse4" class="panel-collapse collapse">
+        <div class="panel-body">Estimated Delivery Time is 10-15 business days for International Deliveries and 5-7 business days for Pakistan.. <br>
+		* Note: The delivery time mentioned above does not include order processing time that usually varies from 1 to 3 days.
+		</div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">Do I get a tracking number? </a>
+        </h4>
+      </div>
+      <div id="collapse5" class="panel-collapse collapse">
+        <div class="panel-body">As soon as you place your order, you will receive a confirmation email with your tracking number and the link for you to track your order.
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">Why isn’t my tracking information not updating? </a>
+        </h4>
+      </div>
+      <div id="collapse6" class="panel-collapse collapse">
+        <div class="panel-body">Tracking information is available within 1-3 days after your order is placed (it takes sometime longer depending on how fast the postal services are processing orders).
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse7">Can I return my order for refund/replacement? </a>
+        </h4>
+      </div>
+      <div id="collapse7" class="panel-collapse collapse">
+        <div class="panel-body">No we do not provide refunds, in case the product is defective or damaged, we will recall the product and get it fixed for you with zero additional charges. 
+		If you have any problems with your items, just email us at support@JewelCircle.net. We will try to find the best method to solve your problems. 
+		</div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse8">If I receive a defective/ damaged product, can I change it? </a>
+        </h4>
+      </div>
+      <div id="collapse8" class="panel-collapse collapse">
+        <div class="panel-body">If the product you received is defective or damaged, please contact us as soon as possible at support@JewelCircle.com.
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse9">What forms of payment do you accept?  </a>
+        </h4>
+      </div>
+      <div id="collapse9" class="panel-collapse collapse">
+        <div class="panel-body">We only accept cash on delivery, Debit Cards, Credit Cards and Direct bank transfers at the moment.</div>
+      </div>
+    </div>
+
+
+  </div> 
+  	<hr>
+	<p>We are so sorry you’re experiencing issues with our product! We know we both want to get you up and running as soon as possible. We would really like to help you by asking you to send us these elements:<p>
+	<ul>
+		<li>Full Name on the order and order number</li>
+		<li>Images & photos of the defective product </li>
+		<li>A brief explanation of the issue</li>
+	</ul>
+	<hr>
+	<p>* Please be advised that if one of the requested elements is missing it will not be possible for us to process your request. 
+	After examining the pictures you sent us, and if the product is damaged, we will proceed to the next step.
+	</p>
+
+</div>
+
+<?php
+  require 'templates/bottom.inc.php';
+?>
