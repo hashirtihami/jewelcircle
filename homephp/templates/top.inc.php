39c7be95137148cb14434f46323e1753399f2b27
Wahaj Hussain <wahajhssn@gmail.com>
2018-08-01 22:09:58 +0500

    lol

--- homephp/templates/top.inc.php
+++ homephp/templates/top.inc.php
@@ -1,78 +1,184 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Jewel Circle</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
-    <!-- bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">      
-    <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-    
-    <link href="themes/css/bootstrappage.css" rel="stylesheet"/>
-    
-    <!-- global styles -->
-    <link href="themes/css/flexslider.css" rel="stylesheet"/>
-    <link href="themes/css/main.css" rel="stylesheet"/>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-    <!-- google font -->
-    <!-- <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet"> -->
-    <link href="https://fonts.googleapis.com/css?family=Dosis|Inconsolata|Pacifico" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="fonts/anydore.css">
-    <!-- scripts -->
-    <script src="themes/js/jquery-1.7.2.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>       
-    <script src="themes/js/superfish.js"></script>  
-    <script src="themes/js/jquery.scrolltotop.js"></script>
-    <!--[if lt IE 9]>     
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-      <script src="js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-    <body>    
-    <div id="top-bar" class="container">
-      <div class="row">
-        <div class="span4">
-          <!-- <img src="logo/JC.png" style="height: 30px;"> -->
-        </div>
-        <div class="span8">
-          <!-- <div class="pull-left heading"><img src="logo/logo.png"></div> -->
-          <a href="index.php"><div class="pull-left heading"><i class="fas fa-crown"></i> Jewel Circle</i></div></a>
-          <div class="account pull-right">
-            <ul class="user-menu">        
-              <li><a href="#"><i class="fas fa-shopping-cart"></i> Your Cart</a></li>
-              <li><a href="loginindex.php"><i class="far fa-user"></i> Login</a></li>    
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="wrapper" class="container">
-      <section class="navbar main-menu">
-        <div class="navbar-inner main-menu">      
-<!--          <a href="index.html" class="logo pull-left">
-            <div class="logo">
-            <img src="logo/JC.png" style="height: 30px;" class="" alt="JC Logo">
-            </div>
-          </a> -->
-          <!-- <a href="index.html" class="logo pull-left"><img src="themes/images/logo.png" class="site_logo" alt=""></a> -->
-          <nav id="menu" class="pull-right">
-            <ul>
-              <li>
-                <a href="./products.html">Bracelet</a>          
-              </li>                             
-              <li>
-                <a href="./products.html">Cufflings</a>
-              </li>
-              <li>
-                <a href="./products.html">Locket</a>
-              </li>
-              <li>
-                <a href="./products.html">Ring</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </section>
+<head>
+	<title>Home 02</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/linearicons-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/MagnificPopup/magnific-popup.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="fonts/anydore/anydore.css">
+</head>
+<body class="animsition">
+	
+	<!-- Header -->
+	<header class="header-v2">
+		<!-- Header desktop -->
+		<div class="container-menu-desktop trans-03">
+			<div class="wrap-menu-desktop">
+				<nav class="limiter-menu-desktop p-l-45">
+					
+					<!-- Logo desktop -->		
+					<a href="#" class="logo">
+						<span class="heading"> Jewel circle</span>
+					</a>
+
+					<!-- Menu desktop -->
+					<div class="menu-desktop">
+						<ul class="main-menu">
+							<li class="active-menu">
+								<a href="index.html">Home</a>
+								
+							</li>
+
+							<li>
+								<a href="product.html">Ring</a>
+							</li>
+
+							<li class="label1" data-label1="50%">
+								<a href="shoping-cart.html">Locket</a>
+							</li>
+
+							<li>
+								<a href="blog.html">Bracelet</a>
+							</li>
+
+							<li>
+								<a href="about.html">Cufflinks</a>
+							</li>
+
+							
+						</ul>
+					</div>	
+
+					<!-- Icon header -->
+					<div class="wrap-icon-header flex-w flex-r-m h-full">
+						<div class="flex-c-m h-full p-r-24">
+							<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
+								<i class="zmdi zmdi-search"></i>
+							</div>
+						</div>
+							
+						<div class="flex-c-m h-full p-l-18 p-r-25 bor5">
+							<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart" data-notify="2">
+								<i class="zmdi zmdi-shopping-cart"></i>
+							</div>
+						</div>
+							
+						<div class="flex-c-m h-full p-lr-19">
+							<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-sidebar">
+								<i class="fas fa-user"></i>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</div>	
+		</div>
+
+		<!-- Header Mobile -->
+		<div class="wrap-header-mobile">
+			<!-- Logo moblie -->		
+			<div class="logo-mobile">
+				<a href="index.html"><img src="images/icons/logo-01.png" alt="IMG-LOGO"></a>
+			</div>
+
+			<!-- Icon header -->
+			<div class="wrap-icon-header flex-w flex-r-m h-full m-r-15">
+				<div class="flex-c-m h-full p-r-10">
+					<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
+						<i class="zmdi zmdi-search"></i>
+					</div>
+				</div>
+
+				<div class="flex-c-m h-full p-lr-10 bor5">
+					<div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart" data-notify="2">
+						<i class="zmdi zmdi-shopping-cart"></i>
+					</div>
+				</div>
+			</div>
+
+			<!-- Button show menu -->
+			<div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+				<span class="hamburger-box">
+					<span class="hamburger-inner"></span>
+				</span>
+			</div>
+		</div>
+
+
+		<!-- Menu Mobile -->
+		<div class="menu-mobile">
+			<ul class="main-menu-m">
+				<li>
+					<a href="index.html">Home</a>
+				
+					<span class="arrow-main-menu-m">
+						<i class="fa fa-angle-right" aria-hidden="true"></i>
+					</span>
+				</li>
+
+				
+							<li>
+								<a href="product.html">Ring</a>
+							</li>
+
+							<li >
+								<a href="shoping-cart.html">Locket</a>
+							</li>
+
+							<li>
+								<a href="blog.html">Bracelet</a>
+							</li>
+
+							<li>
+								<a href="about.html">Cufflinks</a>
+							</li>
+
+			</ul>
+		</div>
+
+		<!-- Modal Search -->
+		<div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
+			<div class="container-search-header">
+				<button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+					<img src="images/icons/icon-close2.png" alt="CLOSE">
+				</button>
+
+				<form class="wrap-search-header flex-w p-l-15">
+					<button class="flex-c-m trans-04">
+						<i class="zmdi zmdi-search"></i>
+					</button>
+					<input class="plh3" type="text" name="search" placeholder="Search...">
+				</form>
+			</div>
+		</div>
+	</header>
