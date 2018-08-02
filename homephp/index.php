d02f6073e1eca72e086f6e6d901971d79f3c6827
tihaami <hashirtihamipk@gmail.com>
2018-08-02 18:29:12 +0500

    posters added

--- homephp/index.php
+++ homephp/index.php
@@ -6,12 +6,12 @@
 	<section class="section-slide">
 		<div class="wrap-slick1 rs1-slick1">
 			<div class="slick1">
-				<div class="item-slick1" style="background-image: url(images/slide-03.jpg);">
+				<div class="item-slick1" style="background-image: url(../assets/images/posters/jc2.jpg);">
 					<div class="container h-full">
 						<div class="flex-col-l-m h-full p-t-100 p-b-30">
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
 								<span class="ltext-202 cl2 respon2">
-									Men Collection 2018
+									Jewel Circle
 								</span>
 							</div>
 								
@@ -30,18 +30,18 @@
 					</div>
 				</div>
 
-				<div class="item-slick1" style="background-image: url(images/slide-02.jpg);">
+				<div class="item-slick1" style="background-image: url(../assets/images/posters/jc3.jpg);">
 					<div class="container h-full">
 						<div class="flex-col-l-m h-full p-t-100 p-b-30">
 							<div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
 								<span class="ltext-202 cl2 respon2">
-									Men New-Season
+									Jewel Circle
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
 								<h2 class="ltext-104 cl2 p-t-19 p-b-43 respon1">
-									Jackets & Coats
+									Name Ring
 								</h2>
 							</div>
 								
