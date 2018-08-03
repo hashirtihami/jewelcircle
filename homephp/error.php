571fa71122e2b5b666bc5d0c56ab14ab09b59397
tihaami <hashirtihamipk@gmail.com>
2018-08-04 02:12:06 +0500

    error.php final

--- homephp/error.php
+++ homephp/error.php
@@ -1,21 +1,24 @@
 <?php
 /* Displays all error messages */
 session_start();
+require 'templates/top.inc.php';
 ?>
 
-
-<div class="form">
-    <h1>Error</h1>
-    <p> 
-    <?php 
-    if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ): 
-        echo $_SESSION['message'];    
-    else:
-        header( "location: index.php" );
-    endif;
-    ?>
-    </p>     
-    <a href="../index.php"><button class="button button-block"/>Home</button></a>
+<div class="container"  style="  text-align:center;">
+    <div class="form">
+        <h1 style="color:black; font-size:6vw; padding-bottom:10px; padding-top:120px;">Oops...  :(</h1>
+        <p> 
+            <?php 
+                    if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ): 
+    
+                echo '<h2 style=" font-size:2vw;"><span style="color:#e60044; "> <i class="fas fa-exclamation-triangle"></i> </span> '.$_SESSION['message'].'&nbsp; <span><a href="loginindex.php"><button style="color:#e60044;  padding-bottom:200px;" class="button button-block"/> Want to log in? </button> </a> </span> <h2>';
+                else:
+                    header( "location: index.php" ); 
+                endif;
+            ?>
+        </p>  
+    </div>
 </div>
-</body>
-</html>
+<?php
+  require 'templates/bottom.inc.php';
+?>
