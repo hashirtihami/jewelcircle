74106591348a4d3a1252ae96258c5e616c49f30f
tihaami <hashirtihamipk@gmail.com>
2018-08-04 20:45:15 +0500

    terms and policy css final

--- homephp/policy.php
+++ homephp/policy.php
@@ -2,49 +2,97 @@
 	require 'templates/top.inc.php';
 ?>
  
- <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style >
+  font{
+    color:#e60044;
+  }
 
-<link href="css/footerlinks.css" rel="stylesheet">
+  label{
+    font-weight:500;
+  }
+
+hr.style14 { 
+  border: 0; 
+  height: 1px; 
+  background-image: -webkit-linear-gradient(left, #f0f0f0, #e60040, #f0f0f0);
+  background-image: -moz-linear-gradient(left, #f0f0f0, #e60040, #f0f0f0);
+  background-image: -ms-linear-gradient(left, #f0f0f0, #e60040, #f0f0f0);
+  background-image: -o-linear-gradient(left, #f0f0f0, #e60040, #f0f0f0); 
+}
+</style>
 
 
-<div class="container">
-  <h3 id="footerheadings">DELIVERY POLICY</h3>
-  <div  class="panel-group">
-    <p > No matter where you live in the World, we deliver our amazing stuff world-wide.
-	You can get your items delivered right to your doorstep. </p>
 
-    <div   class="panel panel-danger">
-      <div class="panel-heading">Our store also delivers to more than 55 cities in Pakistan.</div>
-      <div class="panel-body">Our store delivers to more than 55 cities (Karachi, Islamabad, Lahore, Multan, Faisalabad, Rawalpindi, Sialkot, Hyderabad, Sargodha, Quetta, etc.)</div>
-    </div>
+<div class="container" style="padding-top:50px">
 
-     <div class="panel panel-danger">
-      <div class="panel-heading">Always protected from click to delivery </div>
-      <div class="panel-body">
-      	<ul>
-      		<li>Tracking number provided for all parcels</li>
-      		<li>Full refund if product is not received</li>
-      	</ul>
-      	Estimated Delivery Time is 10-15 business days.
-		Note: The delivery time mentioned above does not include order processing time that usually varies from 01 to 03 days.
-		* Please note that any delivery time stated on our website or given by customer service representatives are estimates only. Please do not cut the delivery times too close. Postal services are not infallible. There may be unexpected delays. Please allow some extra time for un-foreseen delays. We will do our every effort to make sure you receive the parcel in time, but we are not responsible for delivery delays, which can be affected by the shipping carrier, delivery destination, customs clearance, weather, holidays or incorrect/insufficient delivery information.
+  <h2 class="txt-center" style="color:#e60044;"> DELIVERY POLICY</h2>
+  <div style="padding-top:30px">
+    <p class="txt-center" >
+      No matter where you live in the World, we deliver our amazing stuff world-wide.
+      You can get your items delivered right to your doorstep.
+    </p>
 
-      </div>
-    </div>
+    <table class="table table-bordered" >
+    
+      <thead>
+        <tr>
+          <th colspan="4" scope="col"><font>Our store also delivers to more than 55 cities in Pakistan.</font></th>
+        </tr>
+      </thead>
+      <tbody style="margin-bottom:30px">
+        <tr>
+          <td colspan="4">Our store delivers to more than 55 cities (Karachi, Islamabad, Lahore, Multan, Faisalabad, Rawalpindi, Sialkot, Hyderabad, Sargodha, Quetta, etc.)</td>
+        </tr>
+       </tbody>
+    </table>
 
-     <div class="panel panel-danger">
-      	<div class="panel-heading">Track Your Order</div>
-      	<div class="panel-body">It may take up to 02 business days after your order has dispatched for your tracking information to become available online (it might take longer depending on how fast the postal services are processing shipments).
-		We’ll send you the tracking number and the link to track after your order is shipped.
-		</div>
-    </div>
+    <table class="table table-bordered" >
+      <thead>
+          <tr>
+            <th colspan="4" scope="col"><font>Always protected from click to delivery </font></th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+            <td colspan="4">
+            <ul>
+              <li>Tracking number provided for all parcels</li>
+              <li>Full refund if product is not received</li>
+            </ul>
+            Estimated Delivery Time is 10-15 business days.<br>
+             Note: The delivery time mentioned above does not include order processing time that usually varies from 01 to 03 days.
+             <br>
+            <font>*</font> Please note that any delivery time stated on our website or given by customer service representatives are estimates only. Please do not cut the delivery times too close. Postal services are not infallible. There may be unexpected delays. Please allow some extra time for un-foreseen delays. We will do our every effort to make sure you receive the parcel in time, but we are not responsible for delivery delays, which can be affected by the shipping carrier, delivery destination, customs clearance, weather, holidays or incorrect/insufficient delivery information.
 
-     
- 
-   
+            </td>
+          </tr>
+      </tbody>
+    </table>
+    <table class="table table-bordered" >
+        <thead>
+          <tr>
+            <th colspan="4" scope="col"><font>Track Your Order</font></th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+            <td colspan="4">
+              It may take up to 02 business days after your order has dispatched for your tracking information to become available online (it might take longer depending on how fast the postal services are processing shipments).<br>
+              We’ll send you the tracking number and the link to track after your order is shipped.
+
+            </td>
+          </tr>
+      </tbody>
+    </table>
+    
   </div>
 </div>
+
 <?php
   require 'templates/bottom.inc.php';
 ?>
