2d719b68ca8d3b08fdac9947d5c9bd04eb48a2a8
tihaami <hashirtihamipk@gmail.com>
2018-08-05 14:51:02 +0500

    neya neya login/signup form <3

--- homephp/error.php
+++ homephp/error.php
@@ -11,7 +11,7 @@ require 'templates/top.inc.php';
             <?php 
                     if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ): 
     
-                echo '<h2 style=" font-size:2vw;"><span style="color:#e60044; "> <i class="fas fa-exclamation-triangle"></i> </span> '.$_SESSION['message'].'&nbsp; <span><a href="loginindex.php"><button style="color:#e60044;  padding-bottom:200px;" class="button button-block"/> Click here to log in.</button> </a> </span> <h2>';
+                echo '<h2 style=" font-size:2vw;"><span style="color:#e60044; "> <i class="fas fa-exclamation-triangle"></i> </span> '.$_SESSION['message'].'&nbsp; <span><a href="userregister.php"><button style="color:#e60044;  padding-bottom:200px;" class="button button-block"/> Click here to log in.</button> </a> </span> <h2>';
                 else:
                     header( "location: index.php" ); 
                 endif;
