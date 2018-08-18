c0015b9f3ba4fac034b874af3344103270f2e10c
Wahaj Hussain <wahajhssn@gmail.com>
2018-08-18 15:22:16 +0500

    user-issue kinda fixed

--- homephp/payment.php
+++ homephp/payment.php
@@ -1,5 +1,6 @@
 <?php
-require 'templates/top.inc.php';
+require 'db.php';
+session_start();
 if(isset($_POST['email'])&&isset($_POST['firstname'])&&isset($_POST['lastname'])&&isset($_POST['city'])&&isset($_POST['contact'])){
   $_SESSION['email'] = $_POST['email'];
   $_SESSION['firstname'] = $_POST['firstname'];
@@ -9,7 +10,65 @@ if(isset($_POST['email'])&&isset($_POST['firstname'])&&isset($_POST['lastname'])
   $_SESSION['address'] = $_POST['address'];
   $_SESSION['contact'] = $_POST['contact'];
   $_SESSION['state'] = $_POST['state'];
+  $first_name = $mysqli->escape_string($_POST['firstname']);
+  $last_name = $mysqli->escape_string($_POST['lastname']);
+  $email = $mysqli->escape_string($_POST['email']);
+  $password = $mysqli->escape_string(password_hash("NULL", PASSWORD_BCRYPT));
+  $hash = $mysqli->escape_string( md5( rand(0,1000) ) );
+  $contact= $mysqli->escape_string($_POST['contact']);
+  $address= $mysqli->escape_string($_POST['address']);
+  $city= $mysqli->escape_string($_POST['city']);
+  $country= $mysqli->escape_string($_POST['country']);
+  $zipcode= $mysqli->escape_string($_POST['zipcode']);
+  // Check if user with that email already exists
+  $result = $mysqli->query("SELECT * FROM customer WHERE email='$email'") or die($mysqli->error());
+
+  // We know user email exists if the rows returned are more than 0
+  if ( $result->num_rows > 0 ) {
+      
+      $_SESSION['message'] = 'User with this email already exists!'; 
+      if(isset($_SESSION['message']))
+        header("location:error.php");
+  }
+
+  else {
+
+    $sql1 = "INSERT INTO customer (first_name, last_name, email, password, hash, contact, address, city, zipcode, country , role, active) " 
+            . "VALUES ('$first_name','$last_name','$email','$password', '$hash' , '$contact' , '$address','$city','$zipcode','$country' , 'temprorary', '1')";
+
+    // Add user to the database
+    if ( $mysqli->query($sql1) ){
+      $_SESSION['active'] = 1;
+      $_SESSION['logged_in'] = 1;
+      $_SESSION['message'] ='ley kr ley gal';
+
+      $to      = $email;
+      $subject = ' Message from jewelcircle.net ';
+      $message_body = '
+      
+      <div style="text-align:center;">
+              
+              Hello '.$first_name.',<br><br>
+
+              <h3 style="color:#e60044;">Thank you for signing up!</h3><br><br>
+
+              Follow us:<br><br>
+
+              Instagram: https://www.instagram.com/jewel_circle/ <br><br>
+
+              Facebook: https://www.facebook.com/JewelCircle/<br><br>
+
+              For any queries feel free to email us at info@jewelcircle.net
+          </div> ';
+      
+
+      require'mailsender.php';
+    }
+
+  }
+
 }
+require 'templates/top.inc.php';
 ?>
 
 <script src="https://js.stripe.com/v3/"></script>
