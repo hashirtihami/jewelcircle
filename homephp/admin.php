78035fa98d386a6f7bf59399a2785a545a65d7f2
tihaami <hashirtihamipk@gmail.com>
2018-08-19 23:48:42 +0500

    admin login page .. [just front end]

---
+++ homephp/admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="UTF-8" /> 
+    <title>
+        Admin
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/adminhome.css" />
+</head>
+<body>
+
+<form>
+  <h1 style="color:#990000;"> Log in</h1>
+  <div class="inset">
+  <p>
+    <label for="email">email</label>
+    <input autocomplete="off" type="text" name="email" id="email">
+  </p>
+  <p>
+    <label for="password">password</label>
+    <input type="password" name="password" id="password">
+  </p>
+
+  </div>
+  <p class="p-container">
+    <!-- <a href=""><span style="color:#990000;">Forgot password ?</span></a> -->
+    <input style="background-color:#990000;" type="submit" name="go" id="go" value="Log in">
+  </p>
+</form>
+
+</body>
+</html>
