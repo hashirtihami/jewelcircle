69d3a7fa5a3a80eb901d4b435543a11f0729b74c
tihaami <hashirtihamipk@gmail.com>
2018-08-21 18:12:44 +0500

    2CO   initiated

--- homephp/charge.php
+++ homephp/charge.php
@@ -1,122 +1,45 @@
 <?php
-require_once('vendor/autoload.php');
+//require_once('vendor/autoload.php');
 session_start();
 
-  
+Twocheckout::privateKey('4C4F2255-FF88-41B8-88B6-3634484F3F75');
+Twocheckout::sellerId('901248204');
+// Twocheckout::sandbox(true);  #Uncomment to use Sandbox
 
-\Stripe\Stripe::setApiKey('sk_test_8GpMYeW7Nk1C6w3cN8sbfITS');
 try {
-	// SANITIZING POST ARRAY  - IMP SHIT
-	$_POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
-
-	//$email = $_SESSION['email'];
-	$token = $_POST['stripeToken'];
-	// creating customer in stripe only -  no db shit
-	$customer = \Stripe\Customer::create(array(
-		"email" => $_SESSION['email'],
-		"source" => $token
-	));
-
-	//charge customer
-	$charge = \Stripe\Charge::create(array(
-		"amount" => $_SESSION['total'],
-		"currency" => "aud",
-		"description" => "jewelcircle payment",
-		"customer" => $customer->id
-		//'metadata' => ['order_id' => $_SESSION['order_id']],
-	));
-	
-	require 'connect.inc.php';
-	date_default_timezone_set(date_default_timezone_get());
-	$date = date('Y/m/d h:i:s a', time());
-	$email = $_SESSION['email'];
-	$firstName = $_SESSION['first_name'];
-	$lastName = $_SESSION['last_name'];
-	if(isset($_SESSION['products'])){
-		$products = $_SESSION['products'];
-		$totalAmount = $_SESSION['total'];
-		$query = "SELECT customerID FROM customer WHERE email='".$email."'";
-		$query_run = mysqli_query($conn, $query);
-		if($query_array = mysqli_fetch_array($query_run))
-			$customerID = $query_array['customerID'];
-		$query = "INSERT INTO `order` (orderDate, totalAmount, customerID) VALUES ('".$date."', '".$totalAmount."', '".$customerID."')";
-		echo $query;
-		$query_run = mysqli_query($conn, $query);
-		foreach ($products as $key) {
-			$plating = $key['plating'];
-			$language = $key['language'];
-			$nametype = $key['nametype'];
-			$query = "SELECT platingID FROM plating WHERE platingType='".$plating."'";
-			$query_run = mysqli_query($conn, $query);
-			if($query_array = mysqli_fetch_array($query_run))
-				$platingID = $query_array['platingID'];
-			$query = "SELECT languageID FROM language WHERE languageName='".$language."'";
-			$query_run = mysqli_query($conn, $query);
-			if($query_array = mysqli_fetch_array($query_run))
-				$languageID = $query_array['languageID'];
-			$query = "SELECT nameTypeID FROM nametype WHERE nameTypeValue='".$nametype."'";
-			$query_run = mysqli_query($conn, $query);
-			if($query_array = mysqli_fetch_array($query_run))
-				$nameTypeID = $query_array['nameTypeID'];
-			$detailsID = $key['productID'].$languageID.$platingID.$nameTypeID;
-			$query = "INSERT INTO order_product (orderID, productID, detailID, nameOnProduct) VALUES ((SELECT orderID FROM `order` ORDER BY orderID DESC LIMIT 1), '".$key['productID']."', '".$detailsID."', '".$key['nameOnProduct']."')";
-			$query_run = mysqli_query($conn, $query);
-		}
-	unset($_SESSION['products']);
-	unset($_SESSION['total']);
-	$_SESSION['message'] = "Order Placed Successfully";
-	header("location: success.php");
-	}
-	else{
-		$_SESSION['message'] = "Cart is empty";
-		header("location: error.php");
-	}
-}
-
-	catch(\Stripe\Error\Card $e) {
-	  // Since it's a decline, \Stripe\Error\Card will be caught
-	  $body = $e->getJsonBody();
-	  $err  = $body['error'];
-
-	  $_SESSION['message'] = $err['message'];
-	  header('location:error.php');
-	}
-
-
-	  /*
-	  print('Status is:' . $e->getHttpStatus() . "\n");
-	  print('Type is:' . $err['type'] . "\n");
-	  print('Code is:' . $err['code'] . "\n");
-	  // param is '' in this case
-	  print('Param is:' . $err['param'] . "\n");
-	  print('Message is:' . $err['message'] . "\n");
-
-	}
-	catch (\Stripe\Error\RateLimit $e) {
-		  // Too many requests made to the API too quickly
-	} 
-	catch (\Stripe\Error\InvalidRequest $e) {
-		  // Invalid parameters were supplied to Stripe's API
-	} 
-	catch (\Stripe\Error\Authentication $e) {
-		  // Authentication with Stripe's API failed
-		  // (maybe you changed API keys recently)
-	} 
-	catch (\Stripe\Error\ApiConnection $e) {
-		  // Network communication with Stripe failed
-	} 
-	catch (\Stripe\Error\Base $e) {
-		  // Display a very generic error to the user, and maybe send
-		  // yourself an email
-	} 
-	catch (Exception $e) {
-		  // Something else happened, completely unrelated to Stripe
-	}
-	*/
-//redirecting to success
-//header ('Location: paysuccess.php?tid='.$charge->id.'&product='.$charge->description);
-
-// print_r($charge);
-//echo $token;
-
+    $charge = Twocheckout_Charge::auth(array(
+        "merchantOrderId" => "123",
+        "token" => 'Y2U2OTdlZjMtOGQzMi00MDdkLWJjNGQtMGJhN2IyOTdlN2Ni',
+        "currency" => 'USD',
+        "total" => '10.00',
+        "billingAddr" => array(
+            "name" => 'Testing Tester',
+            "addrLine1" => '123 Test St',
+            "city" => 'Columbus',
+            "state" => 'OH',
+            "zipCode" => '43123',
+            "country" => 'USA',
+            "email" => 'testingtester@2co.com',
+            "phoneNumber" => '555-555-5555'
+        ),
+        "shippingAddr" => array(
+            "name" => 'Testing Tester',
+            "addrLine1" => '123 Test St',
+            "city" => 'Columbus',
+            "state" => 'OH',
+            "zipCode" => '43123',
+            "country" => 'USA',
+            "email" => 'testingtester@2co.com',
+            "phoneNumber" => '555-555-5555'
+        )
+    ), 'array');
+    if ($charge['response']['responseCode'] == 'APPROVED') {
+        echo "Thanks for your Order!";
+    }
+} catch (Twocheckout_Error $e) {
+    $e->getMessage();
+}=  
+  
 ?>
+
+<script type="text/javascript" src ="js/charge.js"></script>
--- homephp/js/charge.js
+++ homephp/js/charge.js
@@ -1,68 +1,12 @@
-var stripe = Stripe('pk_test_4fWJrHfLJWUL7r6XWYsOqm5m');
-
-// Create an instance of Elements.
-var elements = stripe.elements();
-
-// Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
-var style = {
-  base: {
-    color: '#32325d',
-    lineHeight: '18px',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4' 
-    }
-  },
-  invalid: {
-    color: '#fa755a',
-    iconColor: '#fa755a'
-  }
+var args = {
+    sellerId: "1817037",
+    publishableKey: "E0F6517A-CFCF-11E3-8295-A7DD28100996",
+    ccNo: $("#ccNo").val(),
+    cvv: $("#cvv").val(),
+    expMonth: $("#expMonth").val(),
+    expYear: $("#expYear").val()
 };
 
-// Create an instance of the card Element.
-var card = elements.create('card', {style: style});
-
-// Add an instance of the card Element into the `card-element` <div>.
-card.mount('#card-element');
-
-// Handle real-time validation errors from the card Element.
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
-
-// Handle form submission.
-var form = document.getElementById('payment-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the user if there was an error.
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server.
-      stripeTokenHandler(result.token);
-    }
-  });
-});
-function stripeTokenHandler(token) {
-  // Insert the token ID into the form so it gets submitted to the server
-  var form = document.getElementById('payment-form');
-  var hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
-  form.appendChild(hiddenInput);
-
-  // Submit the form
-  form.submit();
-}
+TCO.loadPubKey('production', function() {
+    TCO.requestToken(successCallback, errorCallback, args);
+});​
