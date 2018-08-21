f9f2403ecf899a72dbd355aa74ac9c0335b50957
tihaami <hashirtihamipk@gmail.com>
2018-08-21 22:13:12 +0500

    fuck fuckidy fuck

--- homephp/payment.php
+++ homephp/payment.php
@@ -1,10 +1,6 @@
 <?php
   require 'templates/top.inc.php';
-  session_start();
 ?>
-
-<script src="https://js.stripe.com/v3/"></script>
-<link rel="stylesheet"  href="css/payment.css">
 
 <div style="padding-top:50px;" class="container">
   <div class="row">
@@ -15,16 +11,10 @@
         <form action='https://sandbox.2checkout.com/checkout/purchase' method='post'>
           <input type='hidden' name='sid' value='901389480' />
           <input type='hidden' name='mode' value='2CO' />
-          <input type='hidden' name='li_0_type' value='product' />
-          <input type='hidden' name='li_0_name' value='invoice123' />
+          <input type='hidden' name='li_0_name' value='product id' />
           <input type='hidden' name='li_0_price' value='25.99' />
-          <input type='hidden' name='li_0_tangible' value='Y' />
-          <input type='hidden' name='li_1_type' value='shipping' />
-          <input type='hidden' name='li_1_name' value='Express Shipping' />
-          <input type='hidden' name='li_1_price' value='13.99' />
           <input type='hidden' name='card_holder_name' value='Checkout Shopper' />
           <input type='hidden' name='street_address' value='123 Test Address' />
-          <input type='hidden' name='street_address2' value='Suite 200' />
           <input type='hidden' name='city' value='Columbus' />
           <input type='hidden' name='state' value='OH' />
           <input type='hidden' name='zip' value='43228' />
@@ -61,10 +51,7 @@
     </div>
   </div>
 </div>
-<script type="text/javascript" src="js/charge.js"></script>
 <script src="https://www.2checkout.com/static/checkout/javascript/direct.min.js"></script>
-
-
 <?php
   require 'templates/bottom.inc.php';
 ?>
