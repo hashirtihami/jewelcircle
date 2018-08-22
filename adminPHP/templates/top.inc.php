b58d3e0fb8306a4738de2f8c997995f740f3c3a8
tihaami <hashirtihamipk@gmail.com>
2018-08-22 23:10:37 +0500

    front end final.

--- adminPHP/templates/top.inc.php
+++ adminPHP/templates/top.inc.php
@@ -5,6 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="Shortcut Icon" type="image/x-icon" href="../assets/images/logos/favicon2.ico">
   <title>Admin Panel</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
