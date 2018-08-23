1a25a64dbdcf6be1a4598eae77cc3d43e2fd480c
elishaJ <elisha.jamil@gmail.com>
2018-08-23 22:39:16 +0500

    ..

--- homephp/index.php
+++ homephp/index.php
@@ -11,7 +11,7 @@
 			<div class="slick1">
 				<div class="item-slick1" style="background-image: url(../assets/images/posters/jc1.jpg);">
 					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-300 p-b-30">
+						<div class="flex-col-l-m h-full p-t-300 p-b-30 p-t-sm1 p-t-sm2">
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
 								<span class="ltext-202 cl2 respon2">
 									Jewel Circle
@@ -35,7 +35,7 @@
 
 				<div class="item-slick1" style="background-image: url(../assets/images/posters/jc3.jpg);">
 					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-300 p-b-30">
+						<div class="flex-col-l-m h-full p-t-300 p-b-30 p-t-sm1 p-t-sm2">
 							<div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
 								<span class="ltext-202 cl2 respon2">
 									Jewel Circle
@@ -59,7 +59,7 @@
 
 				<div class="item-slick1" style="background-image: url(../assets/images/posters/giftcards.jpg);">
 					<div class="container h-full">
-						<div class="flex-col-l-m h-full p-t-300 p-lr-10-md p-l-25-sm p-b-30">
+						<div class="flex-col-l-m h-full p-t-300 p-b-30 p-t-sm1 p-t-sm2">
 							<div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
 								<span class="ltext-202 cl2 respon2">
 									Make the gift special with a hand made card
