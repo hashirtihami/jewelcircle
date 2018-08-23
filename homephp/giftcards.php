b345cc90721fab46b4c67c9d34d59922c31631aa
tihaami <hashirtihamipk@gmail.com>
2018-08-24 01:59:26 +0500

    chutye!

--- homephp/giftcards.php
+++ homephp/giftcards.php
@@ -6,74 +6,7 @@
 <!-- Product -->
 	<div class="bg0 m-t-23 p-b-140">
 		<div class="container">
-			<div class="flex-w flex-sb-m p-b-52">
-
-				<div class="flex-w flex-c-m m-tb-10">
-					<div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
-						<i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
-						<i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-						 Filter
-					</div>
-				</div>
-
-				<!-- Filter -->
-				<div class="dis-none panel-filter w-full p-t-10">
-					<div class="wrap-filter flex-w w-full p-lr-40 p-t-27 p-lr-15-sm">	
-
-						<div id="filters" class="filter-col2 p-r-15 p-b-27">
-							<div class="mtext-102 cl2 p-b-15">
-								Price
-							</div>
-
-							<ul>
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04 filter-link-active">
-										All
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04" data-filter="two_twofif">
-										Rs200 - Rs250
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04" data-filter="twofif_tri">
-										Rs250 - Rs300
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04" data-filter="tri_trifif">
-										Rs300 - Rs350
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04" data-filter="trifif_for">
-										Rs350 - Rs400
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04" data-filter="for_forfif">
-										Rs400 - Rs450 
-									</a>
-								</li>
-
-								<li class="p-b-6">
-									<a href="#" class="filter-link stext-106 trans-04" data-filter="forfif_fif">
-										Rs450 - Rs500 
-									</a>
-								</li>
-							</ul>
-						</div>
-
-					</div>
-				</div>
-			</div>
-
+			
 
 			<div class="row isotope-grid">
 				<?php
